4230c4db09d82b9d09aa321b20d435653dca67cc
FAIZAN ALI IQBAL ALI <67515374+Faizan2907@users.noreply.github.com>
2022-12-26 19:21:40 +0530

    Add files via upload

---
+++ src/javaapplication1/ChildSession.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Faizan Ali
+ */
+public class ChildSession {
+        public static String username;    
+}
---
+++ src/javaapplication1/LoginSession.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Faizan Ali
+ */
+public class LoginSession {
+    
+    public static String username; 
+    
+}
